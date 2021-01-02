2d7b0306bc479b1b7dabd39cce3ae3d21d7b0dd8
Mike Fair <br8kpoint@gmail.com>
2021-01-02 03:52:53 -0500

    base functionality to work with casrt.io chat

--- youtube.js
+++ youtube.js
@@ -60,13 +60,93 @@ $("body").unbind("click").on("click", "yt-live-chat-text-message-renderer,yt-liv
 
 });
 
+/**
+ * castr chat message integration
+ */
+$("body").unbind("click").on("click", ".chat__message", function () {
+
+  // Don't show deleted messages
+  if($(this)[0].hasAttribute("is-deleted")) {
+    console.log("Not showing deleted message");
+    return;
+  }
+
+  $(".hl-c-cont").remove();
+  var chatname = $(this).find(".chat__message__username").text();
+
+  // Show just the first name. Comment this out to show the full name.
+  //chatname = chatname.replace(/ .*/,'');
+
+  var chatmessage = $(this).clone()    //clone the element
+  .children() //select all the children
+  .remove()   //remove all the children
+  .end()  //again go back to selected element
+  .text();    //get the text of element
+  /*var chatimg = $(this).find("#img").attr('src');
+  chatimg = chatimg.replace("32", "128");
+  var chatdonation = $(this).find("#purchase-amount").html();
+  var chatmembership = $(this).find(".yt-live-chat-membership-item-renderer #header-subtext").html();
+  
+  var chatbadges = "";
+  if($(this).find("#chat-badges .yt-live-chat-author-badge-renderer img").length > 0) {
+    chatbadges = $(this).find("#chat-badges .yt-live-chat-author-badge-renderer img").parent().html();
+  }
+  */
+  $(this).addClass("show-comment");
+
+  /*
+  var hasDonation;
+  if(chatdonation) {
+    hasDonation = '<div class="donation">' + chatdonation + '</div>';
+  } else {
+    hasDonation = '';
+  }
+
+  var hasMembership;
+  if(chatmembership) {
+    hasMembership = '<div class="donation membership">NEW MEMBER!</div>';
+    chatmessage = chatmembership;
+  } else {
+    hasMembership = '';
+  }
+  */
+  var backgroundColor = "";
+  var textColor = "";
+  /*if(this.style.getPropertyValue('--yt-live-chat-paid-message-primary-color')) {
+    backgroundColor = "background-color: "+this.style.getPropertyValue('--yt-live-chat-paid-message-primary-color')+";";
+    textColor = "color: #111;";
+  }
+
+  // This doesn't work yet
+  if(this.style.getPropertyValue('--yt-live-chat-sponsor-color')) {
+    backgroundColor = "background-color: "+this.style.getPropertyValue('--yt-live-chat-sponsor-color')+";";
+    textColor = "color: #111;";
+  }
+  */
+  if($(this).hasClass("chat__message--youtube")) chatimg = "https://chat.castr.io/assets/youtube.png";
+  if($(this).hasClass("chat__message--twitch")){
+     chatimg="https://chat.castr.io/assets/twitch.svg";
+     chatname = $(this).find(".chat__message__username").text();
+     chatmessage = $(this).find(".chat__message__message").text();
+  }
+  if($(this).hasClass("chat__message--facebook")) chatimg="https://chat.castr.io/assets/facebook.png";
+  $( "highlight-chat" ).append('<div class="hl-c-cont fadeout"><div class="hl-name">' + chatname + '</div><div class="hl-message" style="'+backgroundColor+' '+textColor+'">' + chatmessage + '</div><div class="hl-img"><img src="' + chatimg + '"></div>')
+  .delay(10).queue(function(next){
+    $( ".hl-c-cont" ).removeClass("fadeout");
+    next();
+  });
+
+});
+
 $("body").on("click", ".btn-clear", function () {
   $(".hl-c-cont").addClass("fadeout").delay(300).queue(function(){
     $(".hl-c-cont").remove().dequeue();
   });
 });
 
-$( "yt-live-chat-app" ).before( '<highlight-chat></highlight-chat><button class="btn-clear">CLEAR</button>' );
+
+
+$( "yt-live-chat-app, div.chat" ).before( '<highlight-chat></highlight-chat><button class="btn-clear">CLEAR</button>' );
 
 // Show a placeholder message so you can position the window before the chat is live
 $(function(){
