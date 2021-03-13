fbac07accdefe22fd1ea2dc26235aca8c15bf6d5
Mike Fair <br8kpoint@gmail.com>
2021-03-12 21:18:56 -0500

    fix anonymous usr bug and bring in upstrream chagnes

--- youtube.js
+++ youtube.js
@@ -1,11 +1,17 @@
+
 var useFullName = false;  //global variable for using the full name of user or just first name.
 var replaceAnonymous = true; //global variable to replace anonymous users.
 var anonymousUsers = "EmptyUsername" //global variable of comma seprated usernames that will be replaced
 var replaceAnonymousWith = "" //global variable of what any anonymous users will be replaced with
+var highlightWords = [];
 /**
  * binding for youtube chat message click
  */
-$("body").unbind("click").on("click", "yt-live-chat-text-message-renderer,yt-live-chat-paid-message-renderer,yt-live-chat-membership-item-renderer", function () {
+
+
+
+
+$("body").unbind("click").on("click", "yt-live-chat-text-message-renderer,yt-live-chat-paid-message-renderer,yt-live-chat-membership-item-renderer,yt-live-chat-paid-sticker-renderer", function () {
 
   // Don't show deleted messages
   if($(this)[0].hasAttribute("is-deleted")) {
@@ -24,25 +30,34 @@ $("body").unbind("click").on("click", "yt-live-chat-text-message-renderer,yt-liv
   chatimg = chatimg.replace("32", "128");
   var chatdonation = $(this).find("#purchase-amount").html();
   var chatmembership = $(this).find(".yt-live-chat-membership-item-renderer #header-subtext").html();
+  var chatsticker = $(this).find(".yt-live-chat-paid-sticker-renderer #img").attr("src");
+
+  // Donation amounts for stickers use a differnet id than regular superchats
+  if(chatsticker) {
+    chatdonation = $(this).find("#purchase-amount-chip").html();
+  }
+
   var chatbadges = "";
   if($(this).find("#chat-badges .yt-live-chat-author-badge-renderer img").length > 0) {
     chatbadges = $(this).find("#chat-badges .yt-live-chat-author-badge-renderer img").parent().html();
   }
-  $(this).addClass("show-comment");
 
-  var hasDonation;
+  // Mark this comment as shown
+  $(this).addClass("shown-comment");
+
+  var hasDonation = '';
   if(chatdonation) {
     hasDonation = '<div class="donation">' + chatdonation + '</div>';
-  } else {
-    hasDonation = '';
   }
 
-  var hasMembership;
+  var hasMembership = '';
   if(chatmembership) {
     hasMembership = '<div class="donation membership">NEW MEMBER!</div>';
     chatmessage = chatmembership;
-  } else {
-    hasMembership = '';
+  }
+
+  if(chatsticker) {
+    chatmessage = '<img src="'+chatsticker+'">';
   }
 
   var backgroundColor = "";
@@ -59,7 +74,14 @@ $("body").unbind("click").on("click", "yt-live-chat-text-message-renderer,yt-liv
   }
 
 
-  $( "highlight-chat" ).append('<div class="hl-c-cont fadeout"><div class="hl-name">' + chatname + '<div class="hl-badges">' + chatbadges + '</div></div><div class="hl-message" style="'+backgroundColor+' '+textColor+'">' + chatmessage + '</div><div class="hl-img"><img src="' + chatimg + '"></div>'+hasDonation+hasMembership+'</div>')
+  $( "highlight-chat" ).removeClass("preview").append('<div class="hl-c-cont fadeout">'
+     + '<div class="hl-name">' + chatname
+       + '<div class="hl-badges">' + chatbadges + '</div>'
+     + '</div>'
+     + '<div class="hl-message" style="'+backgroundColor+' '+textColor+'">' + chatmessage + '</div>'
+     + '<div class="hl-img"><img src="' + chatimg + '"></div>'
+     +hasDonation+hasMembership
+   +'</div>')
   .delay(10).queue(function(next){
     $( ".hl-c-cont" ).removeClass("fadeout");
     next();
@@ -163,7 +185,7 @@ $( "yt-live-chat-app, div.chat" ).before( '<highlight-chat></highlight-chat><but
 $(function(){
   var chatmessage = "this livestream is the best!";
   var chatimg = "https://pin13.net/youtube-live-chat-sample-avatar.png";
-  $( "highlight-chat" ).append('<div class="hl-c-cont fadeout"><div class="hl-name">Sample User<div class="hl-badges"></div></div><div class="hl-message">' + chatmessage + '</div><div class="hl-img"><img src="' + chatimg + '"></div></div>')
+  $( "highlight-chat" ).addClass("preview").append('<div class="hl-c-cont fadeout"><div class="hl-name">Sample User<div class="hl-badges"></div></div><div class="hl-message">' + chatmessage + '</div><div class="hl-img"><img src="' + chatimg + '"></div></div>')
   .delay(10).queue(function(next){
     $( ".hl-c-cont" ).removeClass("fadeout");
     next();
@@ -174,7 +196,7 @@ $(function(){
 function restoreSettings(){
 
 
-var properties = ["color","authorBackgroundColor","authorColor","commentBackgroundColor","commentColor","fontFamily","fullName","replaceAnonymous","anonymousUsers","replaceAnonymousWith"];
+var properties = ["color","authorBackgroundColor","authorColor","commentBackgroundColor","commentColor","fontFamily","fullName","replaceAnonymous","anonymousUsers","replaceAnonymousWith","highlightWords"];
 chrome.storage.sync.get(properties, function(item){
   var color = "#000000";
   if(item.color) {
@@ -187,6 +209,9 @@ chrome.storage.sync.get(properties, function(item){
   if(item.authorBackgroundColor) {
     root.style.setProperty("--author-bg-color", item.authorBackgroundColor);
     root.style.setProperty("--author-avatar-border-color", item.authorBackgroundColor);
+  }
+  if(item.authorAvatarBorderColor) {
+    root.style.setProperty("--author-avatar-border-color", item.authorAvatarBorderColor);
   }
   if(item.commentBackgroundColor) {
     root.style.setProperty("--comment-bg-color", item.commentBackgroundColor);
@@ -202,8 +227,22 @@ chrome.storage.sync.get(properties, function(item){
   }
   useFullName = item.fullName;
   replaceAnonymous = item.replaceAnonymous;
-  anonymousUsers = item.anonymousUsers;
+  anonymousUsers = item.anonymousUsers || "EmptyUsername"; // default to EmtyUsername for Castr.io but - 2021-01-12
   replaceAnonymousWith = item.replaceAnonymousWith;
+  if(item.scale) {
+    root.style.setProperty("--comment-scale", item.scale);
+  }
+  if(item.commentBottom) {
+    root.style.setProperty("--comment-area-bottom", item.commentBottom);
+  }
+  if(item.commentHeight) {
+    root.style.setProperty("--comment-area-height", item.commentHeight);
+  }
+  if(item.sizeOffset) {
+    root.style.setProperty("--comment-area-size-offset", item.sizeOffset);
+  }
+  highlightWords = item.highlightWords;
+
 });
 }
 restoreSettings();
